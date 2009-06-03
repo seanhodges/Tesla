abd510a5f64e625d1d328badea8d256b90f99f3b
Sean Hodges <sean@token.(none)>
2009-06-04 00:02:46 +0100

    Added power command for KDE4 (will not work on other desktops yet)

--- src/tesla/app/command/provider/GlobalConfigProvider.java
+++ src/tesla/app/command/provider/GlobalConfigProvider.java
@@ -1,8 +1,11 @@
 package tesla.app.command.provider;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import tesla.app.command.Command;
+import tesla.app.command.helper.DBusHelper;
 
 public class GlobalConfigProvider implements IConfigProvider {
 
@@ -18,7 +21,16 @@ public class GlobalConfigProvider implements IConfigProvider {
 
 	private String powerCommand() {
 		// TODO: make this desktop agnostic
-		return "DISPLAY=:0 gnome-session-save --kill --gui";
+		
+		List<String> args = new ArrayList<String>();
+		args.add(new DBusHelper().evaluateArg("-1")); // Confirmation (-1 : user-defined, 0 : immediate, 1 : prompt user)
+		args.add(new DBusHelper().evaluateArg("2")); // Logout type (-1 : previous, 0 : logout, 1 : reboot, 2 : halt)
+		// Session handling (-1 : previous, 0 : wait for other sessions, 1 : cancel if other sessions active, 2 : force, 3 : prompt user)
+		args.add(new DBusHelper().evaluateArg("3"));
+		
+		return new DBusHelper().compileMethodCall("org.kde.ksmserver", "/KSMServer", 
+				"org.kde.KSMServerInterface.logout", args);
+		//return "DISPLAY=:0 gnome-session-save --kill --gui";
 	}
 
 	public Map<String, String> getSettings(String key) {
