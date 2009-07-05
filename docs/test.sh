2bb540814ab67ff811b9ad236c42b3513c5600e8
Sean Hodges <sean@SEAN-PC.(none)>
2009-07-05 19:57:44 +0100

    Improved rhythmdb query, now accepts song info with quotes

--- docs/test.sh
+++ docs/test.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-echo $(qdbus org.gnome.Rhythmbox /org/gnome/Rhythmbox/Player getPlayingUri | sed -e "s/'/\\\'/g")
+#echo $(qdbus org.gnome.Rhythmbox /org/gnome/Rhythmbox/Player getPlayingUri | sed -e "s/'/\\\'/g")
 
-python -c "import libxml2; doc = libxml2.parseFile('/home/sean/.local/share/rhythmbox/rhythmdb.xml'); ctxt = doc.xpathNewContext(); res = ctxt.xpathEval('//entry[@type=\"song\"]/location[.=\"$(qdbus org.gnome.Rhythmbox /org/gnome/Rhythmbox/Player getPlayingUri | sed -e "s/'/\\\'/g")\"]/..'); print res[0]; ctxt.xpathFreeContext(); doc.freeDoc()"
+rhythmdb_path=~/.gnome2/rhythmbox/rhythmdb.xml; if test -e ~/.local/share/rhythmbox/rhythmdb.xml; then rhythmdb_path=~/.local/share/rhythmbox/rhythmdb.xml; fi; uri="$(qdbus org.gnome.Rhythmbox /org/gnome/Rhythmbox/Player getPlayingUri | sed -e "s/'/\\\'/g")"; 
+if [[ ${uri} != "" ]]; then
+python -c "
+import libxml2; 
+doc = libxml2.parseFile('${rhythmdb_path}'); 
+ctxt = doc.xpathNewContext(); 
+res = ctxt.xpathEval('//entry[@type=\"song\"]/location[.=\"${uri}\"]/..'); 
+print res[0]; 
+ctxt.xpathFreeContext(); doc.freeDoc()"
+fi
--- src/tesla/app/command/provider/AppConfigProvider.java
+++ src/tesla/app/command/provider/AppConfigProvider.java
@@ -190,15 +190,20 @@ public class AppConfigProvider implements IConfigProvider {
 				"org.gnome.Rhythmbox.Player.getVolume");
 		}
 		else if (key.equals(Command.GET_MEDIA_INFO)) {
-			out = "python -c \"import libxml2; doc = libxml2.parseFile('/home/sean/.local/share/rhythmbox/rhythmdb.xml'); " +
-					"ctxt = doc.xpathNewContext(); res = ctxt.xpathEval(\\\"//entry[@type='song']/location[.='" +
-					
-					"$(qdbus org.gnome.Rhythmbox /org/gnome/Rhythmbox/Player getPlayingUri | sed -e \"s/\\'/\\\\\\'/g\")" +
-					
-					/*"$(qdbus org.gnome.Rhythmbox /org/gnome/Rhythmbox/Player getPlayingUri)" +*/
-					
-					"']/..\\\"); " +
-					"print res[0]; ctxt.xpathFreeContext(); doc.freeDoc()\"";
+			out = "rhythmdb_path=~/.gnome2/rhythmbox/rhythmdb.xml; " +
+					"if test -e ~/.local/share/rhythmbox/rhythmdb.xml; then " +
+					"rhythmdb_path=~/.local/share/rhythmbox/rhythmdb.xml; " + 
+					"fi; " + 
+					"uri=\"$(qdbus org.gnome.Rhythmbox /org/gnome/Rhythmbox/Player getPlayingUri | sed -e \"s/'/\\\\\\'/g\")\"; " +
+					"if [[ ${uri} != \"\" ]]; then " +
+					"python -c \"" +
+					"import libxml2;" + 
+					"doc = libxml2.parseFile('${rhythmdb_path}');" + 
+					"ctxt = doc.xpathNewContext();" + 
+					"res = ctxt.xpathEval('//entry[@type=\\\"song\\\"]/location[.=\\\"${uri}\\\"]/..');" + 
+					"print res[0];" + 
+					"ctxt.xpathFreeContext(); doc.freeDoc()\"; " +
+					"fi";
 		}
 		
 		return out;
