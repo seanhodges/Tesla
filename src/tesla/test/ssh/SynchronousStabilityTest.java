d558f2bf45154724ccb23fe49f2bb7644c163df8
Sean Hodges <sean@SEAN-PC.(none)>
2009-09-05 21:02:53 +0100

    Added a stress case for SSH commands

---
+++ src/tesla/test/ssh/SynchronousStabilityTest.java
@@ -0,0 +1,62 @@
+package tesla.test.ssh;
+
+import java.util.regex.Pattern;
+
+import java.util.regex.Matcher;
+
+import android.test.AndroidTestCase;
+import tesla.app.command.Command;
+import tesla.app.command.provider.app.AmarokConfig;
+import tesla.app.service.connect.ConnectionException;
+import tesla.app.service.connect.ConnectionOptions;
+import tesla.app.service.connect.SSHConnection;
+
+public class SynchronousStabilityTest extends AndroidTestCase {
+	
+	private static final SSHConnection connection = new SSHConnection();
+	
+	@SuppressWarnings("unused")
+	public void setUp() throws Exception {
+		connection.connect(new ConnectionOptions(this.getContext()) {
+			{
+				this.hostname = "10.0.2.2";
+				this.username = "sean";
+				this.password = "la5TWord5";
+				this.port = 22;
+			}
+			
+			public void reloadSettings() {}
+			public void saveSettings() {}
+		});
+	}
+	
+	public void testDbusCommandSucceedsWhenCalledInSuccession() throws Exception {
+		String commandString = new AmarokConfig().getCommand(Command.GET_MEDIA_INFO);
+		Command command = new Command();
+		command.setCommandString(commandString);
+		
+		Pattern expectedOutput = Pattern.compile("\\[dcop\\]\\n" +
+				"tracknumber:[0-9]*\\n" +
+				"title:[&\\(\\)\\w ]*\\n" +
+				"artist:[\\w ]*\\n" +
+				"album:[\\w ]*\\n" +
+				"[\\S\\s]*");
+		
+		// Send command X times, and check that the output is correct each time
+		for (int i = 0; i < 1000; i++) {
+			String output = null;
+			try {
+				output = connection.sendCommand(command);
+			}
+			catch (ConnectionException e) {
+				fail("Failed at attempt " + (i + 1) + ", error was " + e);
+			}
+			Matcher matcher = expectedOutput.matcher(output);
+			assertTrue("Failed at attempt " + (i + 1) + ", output was " + output, matcher.matches());
+		}
+	}
+	
+	public void tearDown() throws Exception {
+		connection.disconnect();
+	}
+}
